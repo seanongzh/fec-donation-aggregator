ac23fbf1b65e9e4b97a5946f7df3b8d259f172f7
Jarod Gowgiel <jroddg@gmail.com>
2019-09-02 17:39:22 -0600

    Removing an old testing file

--- scripts/FEC_API_demo.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = "http://api.open.fec.gov/v1/committee/C00401224"
-
-print(requests.get(url, params={"api_key": "VDkmeFlFlO9ZRao7AyDyPMrgEeSdwJXO8UdN7faS"}).json()["results"][0]["party"])
